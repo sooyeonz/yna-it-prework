8123d458baf21b6cc07dea03982e41f0feb4cf4d
sooyeonz <5196ksy@naver.com>
2026-05-22 23:28:44 +0900

    init

---
+++ src/main/java/com/yna/itprework/ItPreworkApplication.java
@@ -0,0 +1,13 @@
+package com.yna.itprework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItPreworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItPreworkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yna/itprework/ItPreworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yna.itprework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItPreworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
