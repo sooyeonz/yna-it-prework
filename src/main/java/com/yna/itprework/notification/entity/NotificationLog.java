8b347c506f898756c3ba8654e092b8aa50a1afb3
sooyeonz <5196ksy@naver.com>
2026-05-23 22:10:56 +0900

    feat: NotificationLog 엔티티

---
+++ src/main/java/com/yna/itprework/notification/entity/NotificationLog.java
@@ -0,0 +1,46 @@
+package com.yna.itprework.notification.entity;
+
+import com.yna.itprework.article.CategoryType;
+import com.yna.itprework.user.PushType;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "notification_logs")
+public class NotificationLog {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String deviceId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private PushType pushType;
+
+    @Column(nullable = false)
+    private String articleTitle;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private CategoryType articleCategory;
+
+    @Column(nullable = false)
+    private String status;
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+}
