830edcb6441d764787b052cee3149fb23841b932
sooyeonz <5196ksy@naver.com>
2026-05-23 22:09:19 +0900

    feat: UserCategory 엔티티

---
+++ src/main/java/com/yna/itprework/user/entity/UserCategory.java
@@ -0,0 +1,30 @@
+package com.yna.itprework.user.entity;
+
+import com.yna.itprework.article.CategoryType;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Table(
+        name = "user_categories",
+        uniqueConstraints = @UniqueConstraint(columnNames = {"user_id", "category"})
+)
+public class UserCategory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private CategoryType category;
+
+}
