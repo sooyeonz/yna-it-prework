7deb13af0e934a3c67f2bee8a86530c3bf4bd1be
sooyeonz <5196ksy@naver.com>
2026-05-24 11:00:33 +0900

    feat: 오래된 기사 조회 메서드 페이지네이션 추가

--- src/main/java/com/yna/itprework/article/repository/ArticleRepository.java
+++ src/main/java/com/yna/itprework/article/repository/ArticleRepository.java
@@ -1,6 +1,7 @@
 package com.yna.itprework.article.repository;
 
 import com.yna.itprework.article.entity.Article;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -14,5 +15,5 @@ public interface ArticleRepository extends JpaRepository<Article, String> {
     Set<String> findExistingIds(@Param("ids") Set<String> ids);
 
     @Query("SELECT a.articleId FROM Article a ORDER BY a.pubDate ASC")
-    List<String> findAllArticleIdsSortedByPubDateAsc();
+    List<String> findOldestArticleIds(Pageable pageable);
 }
